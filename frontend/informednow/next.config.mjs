1eddaf001fa54d25193882bb9367b7f0919d8b01
RohithKumar05 <rohithuoft@gmail.com>
2025-11-29 21:34:01 -0500

    Added UI configuration using tailwind styling, downgraded to tailwind v3

--- frontend/informednow/next.config.mjs
+++ frontend/informednow/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+          return [
+              {
+                  source: "/api/:path*", // Matches any request starting with /api/
+                  destination: "http://localhost:3000/api/:path*", // Proxies it to your Express backend
+              },
+          ];
+    }, 
 };
 
 export default nextConfig;
--- frontend/informednow/tailwind.config.js
+++ frontend/informednow/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
