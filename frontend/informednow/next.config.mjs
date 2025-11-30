8346bb7d36010f10b94fb6dacb7b4b174f2f2695
RohithS05 <rohithuoft@gmail.com>
2025-11-30 12:18:07 -0500

    Remove API rewrites from next.config.mjs
    Removed API rewrites configuration from Next.js config.

--- frontend/informednow/next.config.mjs
+++ frontend/informednow/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-          return [
-              {
-                  source: "/api/:path*", // Matches any request starting with /api/
-                  destination: "http://localhost:3000/api/:path*", // Proxies it to your Express backend
-              },
-          ];
-    }, 
+  /* config options here */
 };
 
 export default nextConfig;
