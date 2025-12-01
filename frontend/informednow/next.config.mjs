199e229db2ef462ae844390536104c6e8c5caf6d
devCodesApps <devoak11@gmail.com>
2025-11-30 20:56:47 -0500

    proj-informednow: remove next js config used for local testing

--- frontend/informednow/next.config.mjs
+++ frontend/informednow/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-          return [
-              {
-                  source: "/api/:path*", // Matches any request starting with /api/
-                  destination: "http://localhost:3000/api/:path*", // Proxies it to your Express backend
-              },
-          ];
-    }, 
+
 };
 
 export default nextConfig;
